fe1affd282828d940ebd04e67be92acb945cc34a
Prasanna Keerthipati <prasanna@nbos.vinisoft.com>
2021-10-21 20:57:15 +0530

    Initial Commit

---
+++ src/test/java/com/w2a/Testcase/LoginTest.java
@@ -0,0 +1,29 @@
+package com.w2a.Testcase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+public static WebDriver driver;
+@BeforeSuite
+public void setUp() {
+	driver = new FirefoxDriver();
+	
+}
+@Test
+public void doLogin() {
+	driver.get("http://gmail.com");
+	driver.findElement(By.id("Email")).sendKeys("trainer@way2automation.com");
+	driver.findElement(By.id("password")).sendKeys("sdfdsf");
+}
+@AfterSuite
+public void tearDown() {
+	driver.quit();
+}
+
+}
